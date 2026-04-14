e7c39194eb13452449528bef64a0aa3795ec261e
manuelasolis <manuela.solis@unifesp.br>
2026-04-14 14:45:38 -0300

    Create ex45.c

---
+++ ex45.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main (){
+    float v1, v2;
+
+    printf("v1: ");
+    scanf(%f,&v1)
+        
+    do{
+    prinft("v2: ");
+    scanf(%f,&v2);
+    }
+    
+    while (v2==0);
+    prinft ("%.1f / %1f = %1.f. \v",v1,v2,v1/v2);
+}
